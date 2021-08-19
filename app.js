172d961d0b91641c8b0b05d561d5c89a819f74d1
Soumik Mallik <smkmallik@gmail.com>
2021-08-19 12:05:18 +0530

    feature - Added wiring of the click handler
    Added wiring of the click handler

---
+++ app.js
@@ -0,0 +1,11 @@
+var dateInput = document.querySelector("#date-container");
+var submitButton = document.querySelector("#submit-button");
+
+
+function clickHandler() {
+    console.log('clicked!');
+}
+
+
+
+submitButton.addEventListener('click', clickHandler);
